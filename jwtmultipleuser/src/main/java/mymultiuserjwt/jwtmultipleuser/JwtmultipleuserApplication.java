4d870ea0d0b425934861f43a612841a6b5a0ac69
CindoddCindy <cindodcindy@gmail.com>
2021-01-02 23:53:30 +0700

    tambah pakage ajah

---
+++ jwtmultipleuser/src/main/java/mymultiuserjwt/jwtmultipleuser/JwtmultipleuserApplication.java
@@ -0,0 +1,13 @@
+package mymultiuserjwt.jwtmultipleuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtmultipleuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtmultipleuserApplication.class, args);
+	}
+
+}
---
+++ jwtmultipleuser/src/test/java/mymultiuserjwt/jwtmultipleuser/JwtmultipleuserApplicationTests.java
@@ -0,0 +1,13 @@
+package mymultiuserjwt.jwtmultipleuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtmultipleuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
