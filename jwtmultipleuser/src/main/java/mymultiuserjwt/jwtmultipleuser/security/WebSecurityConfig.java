1c52d3ea9e162378b840e34ac260a40662852ca5
CindoddCindy <cindodcindy@gmail.com>
2021-01-04 09:55:48 +0700

    ga ngapa2in

--- jwtmultipleuser/src/main/java/mymultiuserjwt/jwtmultipleuser/security/WebSecurityConfig.java
+++ jwtmultipleuser/src/main/java/mymultiuserjwt/jwtmultipleuser/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import mymultiuserjwt.jwtmultipleuser.security.jwt.JwtAuthTokenFilter;
 import mymultiuserjwt.jwtmultipleuser.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
